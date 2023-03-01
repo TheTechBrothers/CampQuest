2e48cb3a4d3afeb4f635093172b13a38137bd939
hodgesboyeli <115364128+hodgesboyeli@users.noreply.github.com>
2023-02-28 23:57:11 -0500

    Fixed User and added Event and Search

---
+++ src/main/java/famu/edu/campusquest/Model/Event.java
@@ -0,0 +1,25 @@
+package famu.edu.campusquest.Model;
+
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.protobuf.util.Timestamps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import java.text.ParseException;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Event {
+    @DocumentId
+    private @Nullable String id;
+    private String EventName;
+    private String EventAddress;
+    private String UniversityName;
+    private String EventDate;
+}
+
--- src/main/java/famu/edu/campusquest/Model/User.java
+++ src/main/java/famu/edu/campusquest/Model/User.java
@@ -2,9 +2,13 @@ package famu.edu.campusquest.Model;
 
 import com.google.cloud.Timestamp;
 import com.google.cloud.firestore.annotation.DocumentId;
+import com.google.protobuf.util.Timestamps;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.lang.Nullable;
+
+import java.text.ParseException;
 
 @Data
 @NoArgsConstructor
@@ -12,8 +16,7 @@ import lombok.NoArgsConstructor;
 
 public class User {
     @DocumentId
-
-
+    private @Nullable String id;
     private String FirstName;
     private String LastName;
     private String City;
@@ -21,5 +24,12 @@ public class User {
     private String UniversityName;
     private Timestamp LastLogin;
     private Timestamp CreatedAt;
+
+    public void setLastLogin(String lastLogin) throws ParseException {
+        this.LastLogin = Timestamp.fromProto(Timestamps.parse(lastLogin));
+    }
+    public void setCreatedAt(String createdAt) throws ParseException {
+        this.CreatedAt = Timestamp.fromProto(Timestamps.parse(createdAt));
+    }
 }
 
