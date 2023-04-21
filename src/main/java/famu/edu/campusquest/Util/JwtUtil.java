5d1acb80f842158e152d96b8595440fb6651b8b0
Terrelle Thomas <110430142+TerrelleAThomas@users.noreply.github.com>
2023-04-20 21:00:48 -0400

    yes

--- src/main/java/famu/edu/campusquest/Util/JwtUtil.java
+++ src/main/java/famu/edu/campusquest/Util/JwtUtil.java
@@ -9,6 +9,7 @@ import java.security.Key;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import io.jsonwebtoken.Claims;
 
 public class JwtUtil {
     private static final Key key = Keys.secretKeyFor(io.jsonwebtoken.SignatureAlgorithm.HS256);
@@ -25,11 +26,11 @@ public class JwtUtil {
                 .compact();
     }
 
-    public static Claims getClaimsFromToken(String key) {
-        return Jwts.builder()
-                .setIssuer(key)
+    public static Claims getClaimsFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(token)
                 .build()
-                .parseClaimsJws(key)
+                .parseClaimsJws(token)
                 .getBody();
     }
 }
